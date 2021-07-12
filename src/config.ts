f3a7f60f7cefccdd65b1896ee28489a33c1ea981
Uzochukwu Eddie Odozi <uo@sms-digital.com>
2021-07-12 20:28:09 +0200

    added circleci config

--- src/config.ts
+++ src/config.ts
@@ -21,21 +21,22 @@ class Config {
     public EC2_URL: string | undefined;
 
     private readonly DEFAULT_DATABASE_URL = 'mongodb://localhost:27017/chatapp-backend-test';
+    private readonly DEFAULT_CLIENT_URL = 'http://localhost:4200'
 
     constructor() {
         this.DATABASE_URL = process.env.DATABASE_URL || this.DEFAULT_DATABASE_URL;
-        this.JWT_TOKEN = process.env.JWT_TOKEN || '';
+        this.JWT_TOKEN = process.env.JWT_TOKEN || '1234';
         this.NODE_ENV = process.env.NODE_ENV || '';
         this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || '';
         this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || '';
-        this.CLIENT_URL = process.env.CLIENT_URL || '';
+        this.CLIENT_URL = process.env.CLIENT_URL || this.DEFAULT_CLIENT_URL;
         this.CLOUD_NAME = process.env.CLOUD_NAME || '';
         this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || '';
         this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || '';
         this.REDIS_HOST = process.env.REDIS_HOST || '';
         this.SENDER_EMAIL = process.env.SENDER_EMAIL || '';
         this.SENDER_EMAIL_PASSWORD = process.env.SENDER_EMAIL_PASSWORD || '';
-        this.SENDGRID_API_KEY = process.env.SENDGRID_API_KEY || '';
+        this.SENDGRID_API_KEY = process.env.SENDGRID_API_KEY || 'SG.1234';
         this.EC2_URL = process.env.EC2_URL || '';
     }
 
